85d54446b4a4a044112d1225071a52927c603e95
Hamad Hussain <hhtech007@proton.me>
2025-12-04 02:37:31 +0000

    project completed

---
+++ index.js
@@ -0,0 +1,40 @@
+let encryptBtn = document.getElementById("encrypt-btn");
+let eInput = document.getElementById("encryptedInput-1");
+let pInput = document.getElementById("plainInput-1");
+let inputs = [eInput, pInput];
+let copyBtn = document.getElementById("copyToClipBoard");
+
+inputs.forEach((input) => {
+  input.oninput = () => {
+    input.value = input.value.toUpperCase();
+    // makes all chars uppercase no matter what
+  };
+});
+
+function encrypt() {
+  let pInput = document.getElementById("plainInput-1").value;
+  let solved = "";
+  let shiftInput = parseInt(document.getElementById("shiftInput").value);
+
+  for (let i = 0; i < pInput.length; i++) {
+    let ascii_num = pInput[i].charCodeAt();
+    let sum = ascii_num + shiftInput;
+    sum >= 65 && sum <= 90
+      ? (solved += String.fromCharCode(sum))
+      : sum > 90
+      ? (solved += String.fromCharCode(65 + (sum % 91)))
+      : (solved += pInput[i]);
+  }
+  eInput.value = solved;
+}
+
+function copyText() {
+  eInput.select()
+  eInput.setSelectionRange(0,99999)
+  document.execCommand('copy')
+  alert("Copied to clipboard: "+ eInput.value)
+}
+
+copyBtn.addEventListener('click', copyText)
+encryptBtn.addEventListener('click', encrypt)
+
